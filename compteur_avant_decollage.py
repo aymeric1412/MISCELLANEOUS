9a75a88495afe45f3b7f4215a40c927d93233bab
aymeric <aymeric.petit@icloud.com>
2025-09-29 08:35:17 -0400

    MISC_ADD
    NASA count before taking off

---
+++ compteur_avant_decollage.py
@@ -0,0 +1,15 @@
+import time
+
+i = 10
+
+print("[DÉCOMPTE AVANT DÉCOLLAGE]")
+print("")
+
+while i >= 0:
+    print(i)
+    time.sleep(1)
+    print("")
+    i = i - 1
+
+print("")
+print("DÉCOLLAGE !!")
